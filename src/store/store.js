ff56a7bef16aea7e188532ef8f4a1858b93e7788
Alexander Prisazhny <mutilatorap@gmail.com>
2019-10-23 23:03:34 +0300

    Move to Github Pages

--- src/store/store.js
+++ src/store/store.js
@@ -5,6 +5,23 @@ import thunk from 'redux-thunk';
 import initialState from '../reducers/initialState';
 import rootReducer from '../reducers/rootReducer';
 
+/* const consoleMessages = store => next => action => {
+  let result;
+  console.groupCollapsed(`Dispatching action => ${action.type}`);
+  result = next(action);
+  console.log(`${JSON.stringify(
+    store.getState()
+  )}`)
+
+  console.groupEnd();
+
+  return result;
+} */
+// ^^^^^ 
+// Made this one for the excersise's sake. 
+// You may ucncomment it and add the function's name to 
+// middleware array to check how it works.
+
 const middleware = [thunk];
 
 const store = createStore(
